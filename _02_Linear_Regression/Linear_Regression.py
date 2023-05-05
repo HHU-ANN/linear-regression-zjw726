873e258476013cdfb1f86b65d01cf9a6269c58c2
zjw726 <131933674+zjw726@users.noreply.github.com>
2023-05-05 21:34:35 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -20,9 +20,9 @@ def lasso(data):
     a = 0.01
     ep = 10000
     lr = 0.001
-    w = np.zeros(X.shape[1])
+    w = np.zeros(x.shape[1])
     for i in range(ep):
-        grad = np.matmul(X.T, np.matmul(X, w) - y) / X.shape[0] + a * np.sign(w)
+        grad = np.matmul(x.T, np.matmul(x, w) - y) / x.shape[0] + a * np.sign(w)
         grad_norm = np.linalg.norm(grad)
         if grad_norm > 1:
             grad /= grad_norm
