fa802fb477c670f77a4a3cba26f69c7d0ea9406c
zjw726 <131933674+zjw726@users.noreply.github.com>
2023-05-05 18:06:58 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,16 +8,16 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
-def model(x, y):
+def modeltest(x, y):
     return np.dot(np.linalg.inv(np.dot(x.T,x)),np.dot(x.T,y))
 def ridge(data):
     x, y = read_data()
-    w = model(x, y)
+    w = modeltest(x, y)
     return data @ w
     
 def lasso(data):
     x, y = read_data()
-    w = model(x, y)
+    w = modeltest(x, y)
     return data @ w 
 
                  
